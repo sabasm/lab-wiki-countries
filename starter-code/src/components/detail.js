9b98bb2ebc314c4258b824dc3791781884921975
sabasm <smma1992@gmail.com>
2018-12-12 19:59:47 -0600

    me faltan los set states y los botones

--- starter-code/src/components/detail.js
+++ starter-code/src/components/detail.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Detail = ({name,capital,cioc,area,borders}) =>{
+
+
     return (
         <div className="detail">
         <a href="#" class="list-group-item list-group-item-action active">{cioc}</a>
